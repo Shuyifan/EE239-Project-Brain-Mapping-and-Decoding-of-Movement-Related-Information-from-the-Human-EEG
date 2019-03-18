f206a416f0441438cd1e6a9475d294f4175394fe
yifanshu <466763052@qq.com>
2019-03-18 11:16:09 -0700

    Add shallow net frequency domain

--- Conv_main.py
+++ Conv_main.py
@@ -1,7 +1,9 @@
 import conv_util
 from load import load_data
 from load import load_data_no_EOG
+from load import load_data_with_fq_domain
 
 #data = load_data()
+#data = load_data_with_fq_domain()
 data = load_data_no_EOG()
 conv_util.deep_net(data)
